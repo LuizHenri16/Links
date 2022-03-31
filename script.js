3820f8f098e10ad5368e8e6b653d486a85b35ce5
Luiz Henrique <luizhenrique@pop-os.localdomain>
2022-03-31 10:57:30 -0300

    fix

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const checkBox = document.getElementById('checkbox')
 const bodyClass = document.getElementById('body')
 
+window.addEventListener("load", () => {
+    let message = "Se você entrou aqui, poderia me responder o que você está fazendo aqui? "
+    console.log(message)
+})
+
 checkBox.addEventListener('change', () =>
 {
     document.body.classList.toggle('dark')
 })
+
+
