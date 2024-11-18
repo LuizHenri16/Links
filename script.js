3777a7ad44e5ef94a98ef075fea9ba4561de334c
Luiz Henrique <lbastos722@gmail.com>
2024-11-18 16:38:58 -0300

    mudanças

--- script.js
+++ script.js
@@ -11,4 +11,43 @@ checkBox.addEventListener('change', () =>
     document.body.classList.toggle('dark')
 })
 
+let iconLinks = {
+    github: "https://github.com/LuizHenri16",
+    instagram: "https://www.instagram.com/_._luizhenrique/",
+    linkedin: "https://www.linkedin.com/in/luiz-bastos-395922213/"
+}
 
+let projectLinks = {
+    1: "https://react-app-study-4f1s7no5a-luizhenri16.vercel.app/", 
+    2: "https://great-code-rosy.vercel.app/",
+    3: "https://responsive-teste.vercel.app/",
+    4: "https://login-app-theta.vercel.app/"
+}
+
+function gitHub() {
+    window.open(iconLinks.github)
+}
+
+function instagram() {
+    window.open(iconLinks.instagram)
+}
+
+function linkedin() {
+    window.open(iconLinks.linkedin)
+}
+
+function projectLink1() {
+    window.open(projectLinks[1])
+}
+
+function projectLink2() {
+    window.open(projectLinks[2])
+}
+
+function projectLink3() {
+    window.open(projectLinks[3])
+}
+
+function projectLink4() {
+    window.open(projectLinks[4])
+}
